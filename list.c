5223301a9e51e654b7350c1c8e2d902aaba8e2ce
Enerel <enerel@Enerels-MacBook-Air.local>
2025-07-07 21:50:00 +0900

    done

--- list.c
+++ list.c
@@ -1,54 +1,121 @@
 #include <stdio.h> // 標準入出力ライブラリをインクルード
+#include <stdlib.h>
 
 #define DATATYPE int
 
-typedef struct listNode {
+typedef struct listNode
+{
     DATATYPE data;
-    ListNode *next;
+    struct listNode *next;
 } ListNode;
 
 // 初期化
 // アドレスを受け取り、参照することで関数から直接アクセスする
-void init(ListNode *node) {
+void init(ListNode *node)
+{
     node->next = NULL;
 }
 
 // リストをすべて出力
-int print(ListNode *node) {
+void print(ListNode *node)
+{
     ListNode *now = node->next;
     // 次のノードがNULLになるまで出力
-    printf("list: ")
-    while (now != NULL) {
+    printf("list: ");
+    while (now != NULL)
+    {
         printf("%d ", now->data);
         now = now->next;
     }
     printf("\n");
 }
 
-void append(ListNode *node, DATATYPE data) {
+void append(ListNode *node, DATATYPE data)
+{
+    ListNode *now = node;
+    while (now->next != NULL)
+    {
+        now = now->next;
+    }
+    ListNode *new = (ListNode *)malloc(sizeof(ListNode));
+    new->data = data;
+    new->next = NULL;
+    now->next = new;
     // リストの末尾にノードを挿入する
 }
 
-void insert(ListNode *node, DATATYPE data, int number) {
+void insert(ListNode *node, DATATYPE data, int number)
+{
+    ListNode *now = node;
+    for (int i = 0; i < number && now->next != NULL; i++)
+    {
+        now = now->next;
+    }
+    ListNode *new = (ListNode *)malloc(sizeof(ListNode));
+    new->data = data;
+    new->next = now->next;
+    now->next = new;
     // リストのnumber番目にノードを挿入する
     // プログラミングでは基本0から数え始める
 }
 
-DATATYPE pop(ListNode *node) {
+DATATYPE pop(ListNode *node)
+{
+    if (node->next == NULL)
+        return -1;
+
+    ListNode *prev = node;
+    ListNode *curr = node->next;
+
+    while (curr->next != NULL)
+    {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    DATATYPE val = curr->data;
+    free(curr);
+    prev->next = NULL;
+
+    return val;
     // リストの末尾のdataを取得して削除
 }
 
-DATATYPE dequeue(ListNode *node) {
+DATATYPE dequeue(ListNode *node)
+{
+    if (node->next == NULL)
+        return -1;
+    ListNode *del = node->next;
+    DATATYPE val = del->data;
+    node->next = del->next;
+    free(del);
+    return val;
     // リストの先頭のdataを取得して削除
     // 2番目を先頭にする
 }
 
-DATATYPE delete(ListNode *node, int number) {
+DATATYPE delete(ListNode *node, int number)
+{
+    ListNode *now = node;
+    for (int i = 0; i < number && now->next != NULL; i++)
+    {
+        now = now->next;
+    }
+    if (now->next == NULL)
+        return -1;
+
+    ListNode *del = now->next;
+    DATATYPE val = del->data;
+    now->next = del->next;
+    free(del);
+
+    return val;
     // リストのnumber番目のdataを取得して削除
     // number+1番目をnumber-1番目のnextにつなぐ
 }
 
-int main() {
+int main()
+{
     ListNode list;
 
     init(&list); // アドレスを渡す
@@ -60,12 +127,12 @@ int main() {
     insert(&list, 1, 1); // (list: 5 1 3 8)
 
     printf("delete(2): %d\n", delete(&list, 2)); // delete: 3
-    
+
     printf("pop: %d\n", pop(&list)); // pop: 8
 
     printf("dequeue: %d\n", dequeue(&list)); // dequeue: 5
-    
+
     print(&list); // list: 1
-    
+
     return 0;
 }
