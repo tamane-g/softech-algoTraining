580dbe51e11066b1bee2abe9480e9d1d4e34a9f9
sota128 <176373118+sota128@users.noreply.github.com>
2025-07-08 00:10:33 +0900

    メモリ解放は書くのだろうか

--- list.c
+++ list.c
@@ -1,10 +1,11 @@
 #include <stdio.h> // 標準入出力ライブラリをインクルード
+#include <stdlib.h>
 
 #define DATATYPE int
 
 typedef struct listNode {
     DATATYPE data;
-    ListNode *next;
+    struct listNode *next;
 } ListNode;
 
 // 初期化
@@ -14,10 +15,10 @@ void init(ListNode *node) {
 }
 
 // リストをすべて出力
-int print(ListNode *node) {
-    ListNode *now = node->next;
+void print(ListNode *node) {
+    ListNode *now = node;
     // 次のノードがNULLになるまで出力
-    printf("list: ")
+    printf("list: ");
     while (now != NULL) {
         printf("%d ", now->data);
         now = now->next;
@@ -27,25 +28,76 @@ int print(ListNode *node) {
 
 void append(ListNode *node, DATATYPE data) {
     // リストの末尾にノードを挿入する
+    ListNode *now = node;
+
+    while (now->next != NULL) {
+        now = now->next;
+    }
+    ListNode *newNode = (ListNode *)malloc(sizeof(ListNode));
+    newNode->data = data;
+    newNode->next = NULL;
+    now->next = newNode;
 }
 
 void insert(ListNode *node, DATATYPE data, int number) {
     // リストのnumber番目にノードを挿入する
     // プログラミングでは基本0から数え始める
+    ListNode *now = node;
+    int i = 0;
+
+    while (i < number && now->next != NULL) {
+        now = now->next;
+        i ++;
+    }
+    ListNode *newNode = (ListNode *)malloc(sizeof(ListNode));
+    newNode->data = data;
+    newNode->next = now->next;
+    now->next = newNode;
 }
 
 DATATYPE pop(ListNode *node) {
     // リストの末尾のdataを取得して削除
+    ListNode *prev = NULL;
+    ListNode *now = node;
+    DATATYPE del_data;
+
+    while (now->next != NULL) {
+        prev = now;
+        now = now->next;
+    }
+    prev->next = NULL;
+    del_data = now->data;
+    return del_data;
 }
 
 DATATYPE dequeue(ListNode *node) {
     // リストの先頭のdataを取得して削除
     // 2番目を先頭にする
+    ListNode *first = node;
+    DATATYPE del_data;
+
+    first = node->next;
+    del_data = first->data;
+    node->next = first->next;
+    return del_data;
 }
 
 DATATYPE delete(ListNode *node, int number) {
     // リストのnumber番目のdataを取得して削除
     // number+1番目をnumber-1番目のnextにつなぐ
+    ListNode *prev = NULL;
+    ListNode *now = node;
+    DATATYPE del_data;
+    int i = 0;
+
+    while (i <= number && now != NULL) {
+        prev = now;
+        now = now->next;
+        i ++;
+    }
+    del_data = now->data;
+    prev->next = now->next;
+    return del_data;
 }
 
 int main() {
@@ -60,11 +112,11 @@ int main() {
     insert(&list, 1, 1); // (list: 5 1 3 8)
 
     printf("delete(2): %d\n", delete(&list, 2)); // delete: 3
-    
+
     printf("pop: %d\n", pop(&list)); // pop: 8
 
     printf("dequeue: %d\n", dequeue(&list)); // dequeue: 5
-    
+
     print(&list); // list: 1
     
     return 0;
