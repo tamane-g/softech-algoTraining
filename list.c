25f79ab09406d02cc84c72ae6e2ca091ffc7e395
tamane-g <getianxuan@gmail.com>
2025-07-08 17:04:17 +0900

    [answer] 問題のプログラムがいろいろ間違ってました…混乱させてすみません

--- list.c
+++ list.c
@@ -1,10 +1,11 @@
 #include <stdio.h> // 標準入出力ライブラリをインクルード
+#include <stdlib.h> // メモリ管理のためにstdlib.hライブラリをインクルード
 
 #define DATATYPE int
 
 typedef struct listNode {
     DATATYPE data;
-    ListNode *next;
+    struct listNode *next;
 } ListNode;
 
 // 初期化
@@ -15,9 +16,9 @@ void init(ListNode *node) {
 
 // リストをすべて出力
 int print(ListNode *node) {
-    ListNode *now = node;
+    ListNode *now = node->next;
     // 次のノードがNULLになるまで出力
-    printf("list: ")
+    printf("list: ");
     while (now != NULL) {
         printf("%d ", now->data);
         now = now->next;
@@ -27,12 +28,12 @@ int print(ListNode *node) {
 
 void append(ListNode *node, DATATYPE data) {
     // リストの末尾にノードを挿入する
-    ListNode *now = node;
+    ListNode *new, *now = node;
+    
     while (now->next != NULL) {
         now = now->next;
     }
-    ListNode *new;
-    new = (ListNode *)malloc(sizeof(List)); // メモリの動的確保（ListNode分のメモリを確保する）
+    new = (ListNode *)malloc(sizeof(ListNode)); // メモリの動的確保（ListNode分のメモリを確保する）
     new->data = data;
     new->next = NULL;
     now->next = new;
@@ -41,20 +42,69 @@ void append(ListNode *node, DATATYPE data) {
 void insert(ListNode *node, DATATYPE data, int number) {
     // リストのnumber番目にノードを挿入する
     // プログラミングでは基本0から数え始める
+    ListNode *new, *now = node;
+    
+    for (int i = 0; i < number && now != NULL; i++)
+        now = now->next;
+    
+    new = (ListNode *)malloc(sizeof(ListNode));
+    new->next = now->next;
+    new->data = data;
+    now->next = new;
 }
 
 DATATYPE pop(ListNode *node) {
     // リストの末尾のdataを取得して削除
+    ListNode *prev, *now = node;
+    DATATYPE buf;
+    
+    while (now->next != NULL) {
+        prev = now;
+        now = now->next;
+    }
+    
+    buf = now->data;
+    free(prev->next);
+    prev->next = NULL;
+    
+    return buf;
 }
 
 DATATYPE dequeue(ListNode *node) {
     // リストの先頭のdataを取得して削除
     // 2番目を先頭にする
+    DATATYPE buf = node->next->data;
+    ListNode *prev, *now = node->next;
+    
+    while (now->next != NULL) {
+        now->data = now->next->data;
+        prev = now;
+        now = now->next;
+    }
+    
+    free(prev->next);
+    prev->next = NULL;
+    
+    return buf;
 }
 
 DATATYPE delete(ListNode *node, int number) {
     // リストのnumber番目のdataを取得して削除
     // number+1番目をnumber-1番目のnextにつなぐ
+    DATATYPE buf;
+    ListNode *prev, *now = node->next;
+    
+    for (int i = 0; i < number && now != NULL; i++) {
+        prev = now;
+        now = now->next;
+    }
+    
+    buf = now->data;
+    now = now->next;
+    free(prev->next);
+    prev->next = now;
+    
+    return buf;
 }
 
 int main() {
@@ -67,6 +117,8 @@ int main() {
     append(&list, 8); // (list: 5 3 8)
 
     insert(&list, 1, 1); // (list: 5 1 3 8)
+    
+    print(&list);
 
     printf("delete(2): %d\n", delete(&list, 2)); // delete: 3
     
